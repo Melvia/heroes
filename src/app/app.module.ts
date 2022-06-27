730578d2bc8cf9e71b779a2c074a6a4eaf68eda7
Nata <melvia.nr@gmail.com>
2022-06-27 07:34:10 +0500

    "feat: routing"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserModule,
-    FormsModule    
-  ],
+  declarations: [AppComponent, HeroesComponent, MessagesComponent, DashboardComponent, HeroDetailComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserModule, FormsModule, HttpClientModule,],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
